9b37f418da8c9ea8349a999a832340e2982f383d
Vomchick <vladislav.tavrel@gmail.com>
2025-05-12 23:19:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+namespace Test1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services
+                .AddEndpointsApiExplorer()
+                .AddSwaggerGen()
+                .AddProblemDetails();
+
+            var app = builder.Build();
+
+            // Use registered global error handler
+            app.UseExceptionHandler();
+            app.UseStatusCodePages();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
